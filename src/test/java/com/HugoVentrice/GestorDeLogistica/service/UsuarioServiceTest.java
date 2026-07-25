db7615bd78caf22481b4459678d3c3db86e79e48
Hugo <hugoventricecoll-cpu@users.noreply.github.com>
2026-07-25 02:22:39 +0200

    Add unit tests for UsuarioService with Mockito

--- src/test/java/com/HugoVentrice/GestorDeLogistica/service/UsuarioServiceTest.java
+++ src/test/java/com/HugoVentrice/GestorDeLogistica/service/UsuarioServiceTest.java
@@ -10,7 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,10 +20,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(MockitoExtension.class)
 public class UsuarioServiceTest {
 
-    @Mock
+    @Mock // Una clase falsa, VACÍA, es solo para que el programa no suelte errores
     private UsuarioRepository usuarioRepository;
-    @InjectMocks
+
+    @InjectMocks // Es la clase REAL, con todos los métodos que tiene dentro la clase real [ej: usuarioService.getAllUsuarios()]
     private  UsuarioService usuarioService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    /*
+
+    Mock lo usamos en la "cosa" que obviamos que funciona,
+    es decir Mockeamos las dependencias de la clase que queremos probar.
+         (ej: si queremos probar usuarioService, obviamos que UsuarioRepository funciona, asi que creamos uno falso un "mock" vacío,
+          para que nosotros nos "inventemos" que hace cada método).
+
+    Ya que si usásemos @InjectMocks con todas las cosas (que se PUEDE, excepto con interfaces/objetos que no sean clases)
+    tendrías que también añadir su SUBCAPA... ¡Superineficiente!
+
+     */
 
     @Test
     void getAllUsuarios(){
@@ -40,8 +58,8 @@ public class UsuarioServiceTest {
 
         List<UsuarioDTO> resultado = usuarioService.getAllUsuarios();
 
-        assertThat(resultado).hasSize(1);
-        assertThat(resultado.get(0).getNombre()).isEqualTo("Miguel");
-
+        assertThat(resultado).isNotEmpty();
     }
+
+
 }
